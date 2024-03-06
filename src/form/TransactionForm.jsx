d044fa474ed749643f5a406c346b15a5c4e0d772
Sachin <mittalsam98@gmail.com>
2024-03-06 20:04:21 +0530

    Minor UI fixes

--- src/form/TransactionForm.jsx
+++ src/form/TransactionForm.jsx
@@ -1,42 +1,64 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useTransition } from 'react';
 import axios from 'axios';
 import { API_URL } from '../constants';
 import { TransactionContext } from '../transaction-context';
 
 export default function TransactionForm() {
   const [accountId, setAccountId] = useState('');
-  const [amount, setAmount] = useState();
+  const [isPending, setISPending] = useState(false);
+  const [amount, setAmount] = useState('');
   const { dispatchGetAllTransaction, allTransaction } = useContext(TransactionContext);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    setISPending(true);
     const data = {
       account_id: accountId,
       amount
     };
-    axios.post(API_URL, data).then((response) => {
-      dispatchGetAllTransaction('FETCH_TRANSACTION', {
-        txn: [{ ...response.data }, ...allTransaction]
+    axios
+      .post(API_URL, data)
+      .then((response) => {
+        dispatchGetAllTransaction('FETCH_TRANSACTION', {
+          txn: [{ ...response.data }, ...allTransaction]
+        });
+        setAccountId('');
+        setAmount('');
+      })
+      .finally(() => {
+        setISPending(false);
       });
-    });
   };
   return (
     <div>
-      <form onSubmit={handleFormSubmit}>
-        <label>Account Id</label>
-        <input
-          data-type='account-id'
-          onChange={(e) => setAccountId(e.target.value)}
-          value={accountId}
-        />
-        <label>Amount</label>
-        <input
-          data-type='amount'
-          onChange={(e) => setAmount(e.target.value)}
-          type='number'
-          value={amount}
-        />
-        <input data-type='transaction-submit' disabled={!accountId || !amount} type='submit' />
+      <h3>Transaction Form</h3>
+      <form className='form__container' onSubmit={handleFormSubmit}>
+        <div className='form__container--field'>
+          <label>Account Id</label>
+          <input
+            data-type='account-id'
+            onChange={(e) => setAccountId(e.target.value)}
+            value={accountId}
+          />
+        </div>
+        <div className='form__container--field'>
+          <label>Amount</label>
+          <input
+            data-type='amount'
+            onChange={(e) => setAmount(e.target.value)}
+            type='number'
+            value={amount}
+          />
+        </div>
+        <div className='form__container--field'>
+          <button
+            data-type='transaction-submit'
+            disabled={!accountId || !amount || isPending}
+            type='submit'
+          >
+            {isPending ? 'Submitting...' : 'Submit'}
+          </button>
+        </div>
       </form>
     </div>
   );
